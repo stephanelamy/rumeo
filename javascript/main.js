883963b2f97362808be10f0c228b3f3e4313cec2
Stéphane Lamy <102264388+stephanelamy@users.noreply.github.com>
2022-04-02 22:44:17 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -17,31 +17,6 @@ function mousePressed(){//fonction speciale qui s'active quand on click
   let click = me.mouseWasPressed();
   if (click == "pick_tile"){
     game.pickOneTile(me);
-
-  //check if we select a tile
-  for(let i=0; i < game.tile.length; i++){
-    if(overlap(...game.tile[i].rectangle(), mouseX, mouseY)){
-      game.tile[i].moving = true;
-      game.moving.push(i);
-    }
-  }
-
-  chatbouton();//to close or open chat
-
-  //check if we press on the deck
-  if(game.checkDeck()){
-    game.pickOneTile(game.ourID);
-  }
-
-  //check if we press on sort777
-  if(game.checkSort777()){
-    game.rack[game.ourID].sort(compareTiles777);
-  }
-
-  //check if we press on sort678
-  if(game.checkSort678()){
-    game.rack[game.ourID].sort(compareTiles678);
-  }
 }
 
 function mouseReleased(){//fonction speciale qui s'active quand on relache
