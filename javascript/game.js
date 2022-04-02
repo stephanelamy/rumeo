f7f954f7ea27b9914f45162deaa9b2cae09aa50f
Stephane Lamy <slamy@math.univ-toulouse.fr>
2022-04-02 22:29:08 +0200

    class player

--- javascript/game.js
+++ javascript/game.js
@@ -1,26 +1,14 @@
 class Game{
-  constructor(nbPlayers){
-      this.tile = []; 
-      this.createTiles();//cree toutes les tuiles avec des images JOKERS A AJOUTER PLUS TARD
+  constructor(playerArray){
       this.deck = new Set();
-      for (let i=0; i<this.tile.length; i++){
+      for (let i=0; i<104; i++){ // serait mieux d'éviter ce 104 en dur
         this.deck.add(i);
       }
-      this.rack = []; // array of RackGrid    set goes from 1-n  with 'empty' and n = maximum tiles to be shown(default 20)
-      this.table = new TableGrid(4,16, this.tile);
-      this.nbPlayers = nbPlayers;
-      for (let i = 0; i < nbPlayers; i++){
-          this.rack[i] = new RackGrid(2,10, this.tile);
-      }
-      this.ourID = 0;
-      this.moving = [];//list of moving tiles
+      this.player = playerArray;
       this.pickStartingTiles();
-      this.deckImage = loadImage("png/tile_deck.png");
-      this.image777 = loadImage("png/tile_777.png");
-      this.image678 = loadImage("png/tile_678.png");
   }
 
-  pickOneTile(noPlayer){
+  pickOneTile(player){
     if (this.deck.size == 0){
       return 'empty deck';
     }
@@ -34,139 +22,22 @@ class Game{
         i++;
     }
     this.deck.delete(chosenNo);
-    this.rack[noPlayer].addTile(chosenNo);
+    player.rack.addTile(chosenNo);
     }
 
   pickStartingTiles(){
-    for (let i = 0; i < this.nbPlayers; i++){
+    for (let i = 0; i < this.player.length; i++){
       for (let j = 0; j < 14; j++){
-        this.pickOneTile(i);
+        this.pickOneTile(this.player[i]);
       }
     }
   }
-  
-  createTiles(){
-    // cree les tuiles
-    for (let copie = 1; copie <= 2; copie++){
-      for (let c = 1; c <= 4; c++){
-        for (let n = 1; n <= 13 ; n++){
-          this.tile.push(new Tile(c, n));
-        }
-      }
-    }
-    // chaque tuile connait son numero
-    for (let i = 0; i < this.tile.length; i++){
-      this.tile[i].no = i;
-    }
-  }
 
-  drop(){//drop and/or swap n with wherever it is
-    for(let i = this.moving.length-1;i>=0;i--){//go throught the list backwards so that if we delete a variable we don't cal 2 times
-      let n = this.moving[i];
-      if(overlap(...this.rack[this.ourID].rectangle(),...this.tile[n].center())){
-        this.rack[this.ourID].swap(n);
-      }else if(overlap(...this.table.rectangle(),...this.tile[n].center())){
-        this.table.swap(n);
-      }else{
-        this.tile[n].grid.putTile(n,this.tile[n].row,this.tile[n].col);
-      }
-      this.tile[n].moving = false;
-    }
-    this.moving = [];
-  }
-
-  checkMoving(){//update and draw moving tiles
-    for (const no of game.moving){
-      this.tile[no].x = mouseX; 
-      this.tile[no].y = mouseY;
-      this.tile[no].draw()
-    }
-  }
-
-  checkDeck(){//check if we clicked on the deck
-    return overlap(...this.deckRectangle(),mouseX,mouseY);
-  }
-
-  checkSort777(){//check if we clicked on sort777
-    return overlap(...this.sort777Rectangle(),mouseX,mouseY);
-  }
-
-  checkSort678(){//check if we clicked on sort777
-    return overlap(...this.sort678Rectangle(),mouseX,mouseY);
-  }
-
-  deckCoor(){//deck coor are stocked here in case we want to change them :P
-    let rack = this.rack[this.ourID]//proportinal to be in between the rack and the right side
-    let deckWidth = Tile.computeSize(10);
-    let deckHight = Tile.computeSize(10)*3/2;
-    let deckX = (width + rack.cornerX() + rack.width() - deckWidth)/2;
-    let deckY = rack.cornerY();
-    return [deckX,deckY,deckWidth,deckHight];
-  }
-
-  deckSize(){
-    return Tile.computeSize(10);
-  }
-
-  deckRectangle(){
-    return [this.deckX(), this.deckY(), this.deckSize(), this.deckSize()*3/2];
-  }
-
-  sort777Rectangle(){
-    return [this.deckX(), this.deckY777(), this.deckSize(), this.deckSize()*3/2];
-  }
-
-  sort678Rectangle(){
-    return [this.deckX(), this.deckY678(), this.deckSize(), this.deckSize()*3/2];
-  }
-
-  deckX(){
-    return width - this.deckSize()*1.2;
-  }
-
-  deckY(){
-    return height - 3*(this.deckSize()*(3/2)*1.1);
-  }
-
-  deckY777(){
-    return this.deckY() + this.deckSize()*(3/2)*1.1;
-  }
-
-  deckY678(){
-    return this.deckY() + 2*this.deckSize()*(3/2)*1.1;
-  }
-
-  drawDeck(){
-    // push();
-    // rect(...this.deckCoor());
-    // pop();
-    const size = Tile.computeSize(10);
-    this.deckImage.resize(size, size*3/2);
-    image(this.deckImage, this.deckX(), this.deckY());
-    this.image777.resize(size, size*3/2);
-    image(this.image777, this.deckX(), this.deckY777());
-    this.image678.resize(size, size*3/2);
-    image(this.image678, this.deckX(), this.deckY() + 2*this.deckSize()*(3/2)*1.1 );
-
-  }
-
-  textStatus(){
-    let [isCompletable, isValid] = this.table.parse();
-    let message = 'Completable: ' + isCompletable + '  Valid: ' + isValid;
-    textSize(32);
-    textAlign("center");
-    text(message, width/2, 30);
-  }
-
-  draw(){
-    this.textStatus();
-    this.drawDeck();
-    this.table.draw();
-    this.rack[this.ourID].draw();
-    this.checkMoving();
-  }
 }
   
+
+// MOVE ALL THESE IN UTILS??
+
 // For the moment I don't consider jokers
 // hand is an array of tile numbers
 
---
+++ javascript/player.js
@@ -0,0 +1,162 @@
+class Player{
+    constructor() {
+      this.tile = []; 
+      this.createTiles(); // locally create all tiles and load images JOKERS A AJOUTER PLUS TARD
+      this.rack = new RackGrid(2,10,this.tile);
+      this.table = new TableGrid(4,16,this.tile);
+    }
+
+    createTiles(){
+        for (let copie = 1; copie <= 2; copie++){
+          for (let colour = 1; colour <= 4; colour++){
+            for (let number = 1; number <= 13 ; number++){
+              this.tile.push(new Tile(colour, number));
+            }
+          }
+        }
+        // Each tile knows its own index
+        for (let index = 0; index < this.tile.length; index++){
+          this.tile[index].no = index;
+        }
+      }
+}
+
+
+class HumanPlayer extends Player{
+    constructor() {
+        super();
+        this.deckImage = loadImage("png/tile_deck.png");
+        this.image777 = loadImage("png/tile_777.png");
+        this.image678 = loadImage("png/tile_678.png");
+        this.moving = []; // list of moving tiles
+    }
+
+    // Drawing routines:
+
+    draw(){
+        this.textStatus();
+        this.drawDeck();
+        this.table.draw();
+        this.rack.draw();
+        this.checkMoving();
+      }
+
+    textStatus(){
+      let [isCompletable, isValid] = this.table.parse();
+      let message = 'Completable: ' + isCompletable + '  Valid: ' + isValid;
+      textSize(32);
+      textAlign("center");
+      text(message, width/2, 30);
+    }
+
+    deckWidth(){
+      return Tile.computeSize(10);
+    }
+
+    deckHeight(){
+      return this.deckWidth() * 1.5;
+    }
+
+
+    drawDeck(){
+      image(this.deckImage, this.deckX(), this.deckY(), this.deckWidth(), this.deckHeight());
+      image(this.image777, this.deckX(), this.deckY777(), this.deckWidth(), this.deckHeight());
+      image(this.image678, this.deckX(), this.deckY678(), this.deckWidth(), this.deckHeight());
+    }
+
+    deckX(){
+      return width - this.deckWidth()*1.2;
+    }
+  
+    deckY(){
+      return height - 3 * this.deckHeight() * 1.1;
+    }
+  
+    deckY777(){
+      return this.deckY() + this.deckHeight() * 1.1;
+    }
+  
+    deckY678(){
+      return this.deckY() + 2 * this.deckHeight() * 1.1;
+    }
+
+    checkMoving(){//update and draw moving tiles
+      for (const index of this.moving){
+        this.tile[index].x = mouseX; 
+        this.tile[index].y = mouseY;
+        this.tile[index].draw();
+      }
+    }
+
+    // Clicked mouse routines:
+    
+    mouseWasPressed(){
+      //check if we select a tile
+      for(let i=0; i < this.tile.length; i++){
+        if(overlap(...me.tile[i].rectangle(), mouseX, mouseY)){
+          me.tile[i].moving = true;
+          me.moving.push(i);
+        }
+      }
+
+      chatbouton(); //to close or open chat
+
+      //check if we press on the deck
+      if(this.checkDeck()){
+        return "pick_tile";
+      }
+
+      //check if we press on sort777
+      if(this.checkSort777()){
+        this.rack.sort(compareTiles777);
+      }
+
+      //check if we press on sort678
+      if(this.checkSort678()){
+          this.rack.sort(compareTiles678);
+      }
+    }
+
+    checkDeck(){ //check if we clicked on the deck
+      return overlap(...this.deckRectangle(),mouseX, mouseY);
+    }
+    
+    checkSort777(){ //check if we clicked on sort777
+      return overlap(...this.sort777Rectangle(),mouseX, mouseY);
+    }
+  
+    checkSort678(){//check if we clicked on sort777
+      return overlap(...this.sort678Rectangle(),mouseX, mouseY);
+    }
+
+    deckRectangle(){
+      return [this.deckX(), this.deckY(), this.deckWidth(), this.deckHeight()];
+    }
+  
+    sort777Rectangle(){
+      return [this.deckX(), this.deckY777(), this.deckWidth(), this.deckHeight()];
+    }
+  
+    sort678Rectangle(){
+      return [this.deckX(), this.deckY678(), this.deckWidth(), this.deckHeight()];
+    }  
+
+    // released mouse routine:
+
+    drop(){ //drop and/or swap n with wherever it is
+      for(let i = 0;  i < this.moving.length; i++){ 
+        let n = this.moving[i];
+        if(overlap(...this.rack.rectangle(),...this.tile[n].center())){
+          this.rack.swap(n);
+        }else if(overlap(...this.table.rectangle(),...this.tile[n].center())){
+          this.table.swap(n);
+        }else{
+          this.tile[n].grid.putTile(n,this.tile[n].row,this.tile[n].col);
+        }
+        this.tile[n].moving = false;
+      }
+      this.moving = [];
+    }
+
+    
+}
